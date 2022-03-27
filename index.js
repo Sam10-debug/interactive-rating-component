354fd4d58c684cbca2ecc46a7f5061c4c2b73bfc
Emorinken <emorinkensam@gmail.com>
2022-03-27 22:44:17 +0100

    Enter

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const second= document.querySelector('.second')
 const submit= document.querySelector('.submit')
 const digits= document.querySelectorAll("#digits")
 const select= document.querySelector('.select')
+const arr=[]
 digits.forEach(digit=>{
     digit.onclick=()=>{
     //  console.log(digit.getAttribute('data-value'))
+    arr.push(digit.getAttribute('data-value'))
     select.innerHTML=`You selected ${digit.getAttribute('data-value')} out of 5`
     }
 })
 const submitfunc=()=>{
-    container.classList.toggle("active")
-    second.classList.toggle("active")
+
+    if (arr.length!==0){
+        container.classList.toggle("active")
+        second.classList.toggle("active")
+    }
 }
 submit.addEventListener('click',submitfunc)
